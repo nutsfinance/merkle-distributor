88f4c80efed33fd3f95276e407739cb7e6122856
Shengda Ding <dingshengda@gmail.com>
2022-07-12 01:23:45 -0700

    Read all delta

--- rewards/generate_taiksm_merkle.ts
+++ rewards/generate_taiksm_merkle.ts
@@ -8,20 +8,23 @@ const END_BLOCK = 2082400;
 
 const main = async () => {
     Keyring.loadAll({ type: 'sr25519' });
-    const data = fs.readFileSync('./taiksm_rewards.csv', {encoding:'utf8', flag:'r'});
     const rewardList = new RewardList(CYCLE, START_BLOCK, END_BLOCK);
 
-    for (let line of data.split("\n")) {
-        if (!line) continue;
-        const [address, taiksmAmount, taiAmount] = line.split(",");
-        rewardList.increaseUserRewards(address, "0x0000000000000000000100000000000000000084", taiAmount);
-        rewardList.increaseUserRewards(address, "0x0000000000000000000300000000000000000000", taiksmAmount);
+    for (let i = 0; i <= CYCLE; i++) {
+        const data = fs.readFileSync(`./data/rewards_karura_0_${i}.csv`, {encoding:'utf8', flag:'r'});
+        for (let line of data.split("\n")) {
+            if (!line) continue;
+            const [address, taiksmAmount, taiAmount] = line.split(",");
+            rewardList.increaseUserRewards(address, "0x0000000000000000000100000000000000000084", taiAmount);
+            rewardList.increaseUserRewards(address, "0x0000000000000000000300000000000000000000", taiksmAmount);
+        }
     }
+
     const distribution = rewardList.toMerkleTree();
 
     console.log('Merkle root: ' + distribution.merkleRoot);
     
-    fs.writeFileSync(`./rewards_karura_0_${CYCLE}.json`, JSON.stringify(distribution));
+    fs.writeFileSync(`./merkle/rewards_karura_0_${CYCLE}.json`, JSON.stringify(distribution));
 }
 
 main();
