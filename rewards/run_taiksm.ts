7729e0b4762b98cd88a9c959d90e95817a6f6311
Shengda Ding <dingshengda@gmail.com>
2022-08-08 16:23:46 -0700

    Submit merkle tree

--- rewards/run_taiksm.ts
+++ rewards/run_taiksm.ts
@@ -2,6 +2,7 @@ import { distributeTaiKsm } from "./distribute-taiksm";
 import { generateMerkle } from "./generate_merkle";
 import { getAccounts } from "./query-accounts";
 import { getTaiKsmRawBalance, getTaiKsmBalance } from "./query-balance-taiksm";
+import { submitMerkle } from "./submit-merkle";
 
 const BLOCK = 2372400;
 
@@ -12,6 +13,7 @@ const main = async () => {
         await getTaiKsmBalance(BLOCK);
         await distributeTaiKsm(BLOCK);
         await generateMerkle("taiksm", BLOCK);
+        await submitMerkle("taiksm");
 
         process.exit(0);
     } catch(error) {
---
+++ rewards/submit-merkle.ts
@@ -0,0 +1,58 @@
+import { calcEthereumTransactionParams } from "@acala-network/eth-providers";
+import { ethers } from "ethers";
+import { abi } from "./merkle-distributor.abi";
+import { CONFIG } from "./config";
+import * as fs from 'fs';
+
+export const submitMerkle = async (asset: string) => {
+
+    // Get the current cycle
+    const provider = new Provider({
+        provider: new WsProvider("wss://karura.api.onfinality.io/public-ws") 
+    });
+    await provider.api.isReady;
+    const merkleDistributor = new ethers.Contract(CONFIG[asset].merkleDistributor, abi, provider);
+    const currentCycle = (await merkleDistributor.currentCycle()).toNumber();
+    const newMerkleFile = __dirname + `/data/merkle/${CONFIG[asset].network}_${CONFIG[asset].poolId}_${currentCycle + 1}.json`;
+    const newMerkle = fs.readFileSync(newMerkleFile, {encoding:'utf8', flag:'r'});
+    const newMerkleTree = JSON.parse(newMerkle);
+
+    const blockNumber = await provider.getBlockNumber();
+    const storageByteDeposit = CONFIG[asset].network === 'acala' ? "300000000000000" : "100000000000000";
+    const ethParams = calcEthereumTransactionParams({
+        gasLimit: '800000',
+        validUntil: (blockNumber + 100).toString(),
+        storageLimit: '600',
+        txFeePerGas: '199999946752',
+        storageByteDeposit
+    });
+
+    const tx1 = await merkleDistributor.proposeRoot(newMerkleTree.merkleRoot, ethers.utils.formatBytes32String(''), currentCycle + 1, newMerkleTree.startBlock, newMerkleTree.endBlock, {
+        gasPrice: ethParams.txGasPrice,
+        gasLimit: ethParams.txGasLimit,
+    });
+    await tx1.wait();
+
+    console.log('Cycle after proposal: ' + await merkleDistributor.currentCycle());
+    console.log('Pending cycle: ' + await merkleDistributor.pendingCycle());
+    console.log('Pending Merkle root: ' + await merkleDistributor.pendingMerkleRoot());
+    console.log('Pending Merkle content hash: ' + await merkleDistributor.pendingMerkleContentHash());
+    console.log('Last proposed start block: ' + await merkleDistributor.lastProposeStartBlock());
+    console.log('Last proposed end block: ' + await merkleDistributor.lastProposeEndBlock());
+    console.log('Last proposed timestamp: ' + await merkleDistributor.lastProposeTimestamp());
+    console.log('Last proposed block number: ' + await merkleDistributor.lastProposeBlockNumber());
+
+    const tx2 = await merkleDistributor.approveRoot(newMerkleTree.merkleRoot, ethers.utils.formatBytes32String(''), currentCycle + 1, newMerkleTree.startBlock, newMerkleTree.endBlock, {
+        gasPrice: ethParams.txGasPrice,
+        gasLimit: ethParams.txGasLimit,
+    });
+    await tx2.wait();
+
+    console.log('Cycle after approval: ' + await merkleDistributor.currentCycle());
+    console.log('Merkle root: ' + await merkleDistributor.merkleRoot());
+    console.log('Merkle content hash: ' + await merkleDistributor.merkleContentHash());
+    console.log('Last publish start block: ' + await merkleDistributor.lastPublishStartBlock());
+    console.log('Last publish end block: ' + await merkleDistributor.lastPublishEndBlock());
+    console.log('Last publish timestamp: ' + await merkleDistributor.lastPublishTimestamp());
+    console.log('Last publish block number: ' + await merkleDistributor.lastPublishBlockNumber());
+}
