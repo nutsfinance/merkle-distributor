9cdd85d0ea8b5255dfdcefae3f11242a5d4528cc
Shengda Ding <dingshengda@gmail.com>
2022-08-11 15:40:36 -0700

    Run taiKSM pipeline

--- rewards/run_taiksm.ts
+++ rewards/run_taiksm.ts
@@ -1,30 +1,41 @@
+import { ethers } from "hardhat";
+
 import { distributeTaiKsm } from "./distribute-taiksm";
 import { generateMerkle } from "./generate_merkle";
 import { getAccounts } from "./query-accounts";
 import { getTaiKsmRawBalance, getTaiKsmBalance } from "./query-balance-taiksm";
 import { submitMerkle } from "./submit-merkle";
 
-const BLOCK = 2372400;
-
-const main = async () => {
+const runTaiKsmPipeline = async (block: number) => {
     try {
         // Common
-        await getAccounts('karura', BLOCK);
+        await getAccounts('karura', block);
 
         // Asset-specific
-        await getTaiKsmRawBalance(BLOCK);
-        await getTaiKsmBalance(BLOCK);
-        await distributeTaiKsm(BLOCK);
+        await getTaiKsmRawBalance(block);
+        await getTaiKsmBalance(block);
+        await distributeTaiKsm(block);
 
         // Common
-        await generateMerkle("taiksm", BLOCK);
-        await submitMerkle("taiksm");
-
-        process.exit(0);
+        await generateMerkle("taiksm", block);
+        // await submitMerkle("taiksm");
     } catch(error) {
         console.error(error);
         process.exit(1);
     }
 }
 
-main();
+const main = async () => {
+    const blockNumber = await ethers.provider.getBlockNumber();
+    // Round down to nearest 100 blocks
+    const block = Math.floor(blockNumber / 100) * 100;
+
+    await runTaiKsmPipeline(block);
+}
+
+main().then(() => {
+    process.exit(0);
+}).catch(error => {
+    console.error(error);
+    process.exit(1);
+})
