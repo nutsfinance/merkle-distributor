8358a9d0f100d077d4482f21df6460f8e3f416fe
Shengda Ding <dingshengda@gmail.com>
2022-08-19 01:02:28 -0700

    Add LKSM

--- rewards/config.ts
+++ rewards/config.ts
@@ -9,6 +9,11 @@ export const CONFIG: any = {
         poolId: 1,
         merkleDistributor: "0xff066331be693BE721994CF19905b2DC7475C5c9"
     },
+    "lksm": {
+        network: "karura",
+        poolId: 2,
+        merkleDistributor: "0xc907CE08ac3f1AaD3AB0Adce5A20c907334C09B3"
+    },
     "tdot": {
         network: "acala",
         poolId: 0,
