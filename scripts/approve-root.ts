139caf258076295f8ddecb962a2dba5746d667e4
Frank Yin <ukby.1234@gmail.com>
2022-12-26 13:42:18 +0800

    a

--- scripts/approve-root.ts
+++ scripts/approve-root.ts
@@ -1,15 +1,15 @@
 import { calcEthereumTransactionParams } from "@acala-network/eth-providers";
 import { ethers } from "hardhat";
 
-//const DISTRIBUTOR = '0xff066331be693BE721994CF19905b2DC7475C5c9'; //3usd
-// const DISTRIBUTOR = '0xf595F4a81B27E5CC1Daca349A69c834f375224F4'; //taiKSM
-// const DISTRIBUTOR = '0x219FA396aE50f789b0cE5e27D6EcbE6b36ef49d9'; //tdot
-const DISTRIBUTOR = '0xc907CE08ac3f1AaD3AB0Adce5A20c907334C09B3'; // lksm
+const DISTRIBUTOR = '0xff066331be693BE721994CF19905b2DC7475C5c9'; //3usd
+ //const DISTRIBUTOR = '0xf595F4a81B27E5CC1Daca349A69c834f375224F4'; //taiKSM
+ //const DISTRIBUTOR = '0x219FA396aE50f789b0cE5e27D6EcbE6b36ef49d9'; //tdot
+//const DISTRIBUTOR = '0xc907CE08ac3f1AaD3AB0Adce5A20c907334C09B3'; // lksm
 // const DISTRIBUTOR = '0x73D6df4395CD54DF2E07fD3880c1B47Aeb2Aed97'; // ldot
-const NEW_ROOT = '0x0000000000000000000000000000000000000000000000000000000000000000';
-const NEW_CYCLE = 1;
-const NEW_START_BLOCK = 0;
-const NEW_END_BLOCK = 2454000;
+const NEW_ROOT = '0x6d0bf3da406bd7c6036ca3cb150c65014bd0e4ddbfdbaf9a06a097020da7610c';
+const NEW_CYCLE = 21;
+const NEW_START_BLOCK = 2487600;
+const NEW_END_BLOCK = 2558200;
 
 async function main() {
     const blockNumber = await ethers.provider.getBlockNumber();
@@ -22,16 +22,17 @@ async function main() {
         storageByteDeposit: '300000000000000'
     });
     const [deployer] = await ethers.getSigners();
+    console.log(deployer);
 
     const MerkleDistributor = await ethers.getContractFactory("MerkleDistributor");
     const distributor = MerkleDistributor.attach(DISTRIBUTOR);
     console.log('Cycle before: ' + await distributor.currentCycle());
 
-    const tx1 = await distributor.approveRoot(NEW_ROOT, ethers.utils.formatBytes32String(''), NEW_CYCLE, NEW_START_BLOCK, NEW_END_BLOCK, {
-        gasPrice: ethParams.txGasPrice,
-        gasLimit: ethParams.txGasLimit,
-    });
-    await tx1.wait();
+    // const tx1 = await distributor.approveRoot(NEW_ROOT, ethers.utils.formatBytes32String(''), NEW_CYCLE, NEW_START_BLOCK, NEW_END_BLOCK, {
+    //     gasPrice: ethParams.txGasPrice,
+    //     gasLimit: ethParams.txGasLimit,
+    // });
+    // await tx1.wait();
 
     console.log('Cycle after: ' + await distributor.currentCycle());
     console.log('Merkle root: ' + await distributor.merkleRoot());
