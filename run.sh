cdf9ccdada13bbc2d85dd04f9341ad44bfad7c6e
Frank Yin <ukby.1234@gmail.com>
2023-02-27 11:28:49 -0800

    correct exit code

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ DATE=$(date '+%Y-%m-%d-%H')
 export LOG_FILE=${DATE}_stdout.log
 export ERROR_LOG_FILE=${DATE}_stderr.log
 ./distribute.sh 2>${ERROR_LOG_FILE} | tee ${LOG_FILE}
+EXIT_CODE=$?
 npx hardhat run rewards/upload_log.ts --network acala
+exit $EXIT_CODE
