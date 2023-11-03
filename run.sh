ecd00abc11c6699017c53577a4f356384450fdaf
Frank Yin <ukby.1234@gmail.com>
2023-11-03 14:00:02 -0700

    cat error files

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ export ERROR_LOG_FILE=${DATE}_stderr.log
 ./distribute.sh 2>${ERROR_LOG_FILE} | tee ${LOG_FILE}
 EXIT_CODE=$?
 npx hardhat run rewards/upload_log.ts --network acala
+cat ${ERROR_LOG_FILE}
 exit $EXIT_CODE
