bfc9e220d67f6d56ef603abbdaeae17866f1023c
Frank Yin <ukby.1234@gmail.com>
2024-01-11 16:27:53 +0700

    Revert "test"
    This reverts commit 200f2de44d6ec9a286369adb5888a51baa37ba89.

--- distribute.sh
+++ distribute.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-#npx hardhat run rewards/run_tdot.ts --network acala
-#npx hardhat run rewards/run_taiksm.ts --network karura
+npx hardhat run rewards/run_tdot.ts --network acala
+npx hardhat run rewards/run_taiksm.ts --network karura
 npx hardhat run rewards/run_3usd.ts --network karura
