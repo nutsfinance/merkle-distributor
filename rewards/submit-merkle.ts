f0f04c5456c2aab14ecea616b3185b6246243430
Crypto Dev <ci@nuts.finance>
2024-11-19 22:03:18 -0800

    use transaction for merkle

--- rewards/submit-merkle.ts
+++ rewards/submit-merkle.ts
@@ -3,7 +3,6 @@ import { WsProvider } from "@polkadot/api";
 import { BodhiProvider } from "@acala-network/eth-providers";
 import { ethers } from "ethers";
 import { merkletDistributorAbi } from "./merkle-distributor.abi";
-import { rewardCollectorAbi } from "./reward-collector.abi";
 import { rewardCollectorAggregatorAbi } from "./reward-collector-aggregator.abi";
 import { CONFIG } from "./config";
 import { getFile } from "./lib/aws_utils";
@@ -22,7 +21,7 @@ export const submitMerkle = async (asset: string, automated: boolean) => {
         });
     } else {
         provider = new BodhiProvider({
-            provider: new WsProvider("wss://acala-rpc-3.aca-api.network/ws") 
+            provider: new WsProvider("wss://acala-rpc-3.aca-api.network/ws")
         });
     }
     const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC!).connect(provider);
@@ -77,90 +76,53 @@ export const submitMerkle = async (asset: string, automated: boolean) => {
 
     console.log(`Proposing cycle: ${currentCycle + 1}: root = ${newMerkleTree.merkleRoot}, start = ${newMerkleTree.startBlock}, end = ${newMerkleTree.endBlock}`);
 
-    const tx1 = await merkleDistributor.proposeRoot(newMerkleTree.merkleRoot, ethers.utils.formatBytes32String(''), currentCycle + 1, newMerkleTree.startBlock, newMerkleTree.endBlock, {
+    console.log(`Reward collector aggregator address: ${CONFIG[asset].aggregator}`);
+    const aggregator = new ethers.Contract(CONFIG[asset].aggregator, rewardCollectorAggregatorAbi, wallet);
+    const roleAddress = '0x99537d82F6F4AAD1419dD14952B512c7959A2904';
+    const role1 = await aggregator.DISTRIBUTOR_ROLE();
+    console.log('Proposal role: ' + role1)
+    console.log('Has role: ' + await aggregator.hasRole(role1, roleAddress));
+    const tx2 = await aggregator.proposeAndDistribute(
+        newMerkleTree.merkleRoot, currentCycle + 1, newMerkleTree.startBlock, newMerkleTree.endBlock,
+        CONFIG[asset].merkleDistributor, CONFIG[asset].rewardCollectorForFee,
+        CONFIG[asset].rewardCollectorForOther,
+        feeTokens, feeAmounts, otherTokens, otherAmounts, {
         gasPrice: ethParams.txGasPrice,
         gasLimit: ethParams.txGasLimit,
     });
-    await tx1.wait();
+    await tx2.wait();
+    console.log("all rewards distributed");
 
-    console.log('Cycle after proposal: ' + await merkleDistributor.currentCycle());
-    console.log('Pending cycle: ' + await merkleDistributor.pendingCycle());
-    console.log('Pending Merkle root: ' + await merkleDistributor.pendingMerkleRoot());
-    console.log('Pending Merkle content hash: ' + await merkleDistributor.pendingMerkleContentHash());
-    console.log('Last proposed start block: ' + await merkleDistributor.lastProposeStartBlock());
-    console.log('Last proposed end block: ' + await merkleDistributor.lastProposeEndBlock());
-    console.log('Last proposed timestamp: ' + await merkleDistributor.lastProposeTimestamp());
-    console.log('Last proposed block number: ' + await merkleDistributor.lastProposeBlockNumber());
+    console.log('Cycle after approval: ' + await merkleDistributor.currentCycle());
+    console.log('Merkle root: ' + await merkleDistributor.merkleRoot());
+    console.log('Merkle content hash: ' + await merkleDistributor.merkleContentHash());
+    console.log('Last publish start block: ' + await merkleDistributor.lastPublishStartBlock());
+    console.log('Last publish end block: ' + await merkleDistributor.lastPublishEndBlock());
+    console.log('Last publish timestamp: ' + await merkleDistributor.lastPublishTimestamp());
+    console.log('Last publish block number: ' + await merkleDistributor.lastPublishBlockNumber());
 
-
-    if (automated) {
-        if (CONFIG[asset].aggregator) {
-            console.log(`Reward collector aggregator address: ${CONFIG[asset].aggregator}`);
-            const aggregator = new ethers.Contract(CONFIG[asset].aggregator, rewardCollectorAggregatorAbi, wallet);
-            const roleAddress = '0x99537d82F6F4AAD1419dD14952B512c7959A2904';
-            const role1 = await aggregator.DISTRIBUTOR_ROLE();
-            console.log('Proposal role: ' + role1)
-            console.log('Has role: ' + await aggregator.hasRole(role1, roleAddress));
-            const tx2 = await aggregator.distribute(CONFIG[asset].merkleDistributor, CONFIG[asset].rewardCollectorForFee, 
-                CONFIG[asset].rewardCollectorForOther,
-                feeTokens, feeAmounts, otherTokens, otherAmounts, {
-                gasPrice: ethParams.txGasPrice,
-                gasLimit: ethParams.txGasLimit,
-            });
-            await tx2.wait();
-            console.log("all rewards distributed");
-        } else {
-            console.log(`Reward collector address for fee/yield: ${CONFIG[asset].rewardCollectorForFee}`);
-            const rewardCollector = new ethers.Contract(CONFIG[asset].rewardCollectorForFee, rewardCollectorAbi, wallet);
-            const roleAddress = '0x99537d82F6F4AAD1419dD14952B512c7959A2904';
-            const role1 = await rewardCollector.DISTRIBUTOR_ROLE();
-            console.log('Proposal role: ' + role1)
-            console.log('Has role: ' + await rewardCollector.hasRole(role1, roleAddress));
-            const tx2 = await rewardCollector.distribute(CONFIG[asset].merkleDistributor, feeTokens, feeAmounts, {
-                gasPrice: ethParams.txGasPrice,
-                gasLimit: ethParams.txGasLimit,
-            });
-            await tx2.wait();
-            console.log("fee/yield distributed");
-        }
-
-        const tx3 = await merkleDistributor.approveRoot(newMerkleTree.merkleRoot, ethers.utils.formatBytes32String(''), currentCycle + 1, newMerkleTree.startBlock, newMerkleTree.endBlock, {
-            gasPrice: ethParams.txGasPrice,
-            gasLimit: ethParams.txGasLimit,
-        });
-        await tx3.wait();
-
-        console.log('Cycle after approval: ' + await merkleDistributor.currentCycle());
-        console.log('Merkle root: ' + await merkleDistributor.merkleRoot());
-        console.log('Merkle content hash: ' + await merkleDistributor.merkleContentHash());
-        console.log('Last publish start block: ' + await merkleDistributor.lastPublishStartBlock());
-        console.log('Last publish end block: ' + await merkleDistributor.lastPublishEndBlock());
-        console.log('Last publish timestamp: ' + await merkleDistributor.lastPublishTimestamp());
-        console.log('Last publish block number: ' + await merkleDistributor.lastPublishBlockNumber());
-
-        if (asset == "tdot") {
-            const claimAddress = '5EMjsd17QbANZBFZMkSfPbvNpijT1qR9r5BwoaoNV6deeRFo';
-            const evmAddress = '0x65766d3ae1bd4306a178f86a9214c39abcd53d021bedb0f90000000000000000';
-            const claim = newMerkleTree.claims[claimAddress];
-            if (claim) {
-                const tx4 = await merkleDistributor.claim(evmAddress, claim.tokens, claim.cumulativeAmounts, claim.index, claim.cycle, claim.proof, {
-                    gasPrice: ethParams.txGasPrice,
-                    gasLimit: ethParams.txGasLimit,
-                });
-                await tx4.wait();
-                console.log('Claimed tDOT for ' + claimAddress);
-            }
-        }
-        const claimAddress = '5ChQuE91nkwu2C2LF3j8BUgBfCcrMR7CLDaR9rvLmyZLJ7hq';
-        const evmAddress = '0x1bff005918ab80f8f3b16fdd0722654a62aeb0d2ffd11b895fc305a77dee6933';
+    if (asset == "tdot") {
+        const claimAddress = '5EMjsd17QbANZBFZMkSfPbvNpijT1qR9r5BwoaoNV6deeRFo';
+        const evmAddress = '0x65766d3ae1bd4306a178f86a9214c39abcd53d021bedb0f90000000000000000';
         const claim = newMerkleTree.claims[claimAddress];
         if (claim) {
             const tx4 = await merkleDistributor.claim(evmAddress, claim.tokens, claim.cumulativeAmounts, claim.index, claim.cycle, claim.proof, {
                 gasPrice: ethParams.txGasPrice,
-                    gasLimit: ethParams.txGasLimit,
+                gasLimit: ethParams.txGasLimit,
             });
             await tx4.wait();
             console.log('Claimed tDOT for ' + claimAddress);
         }
+    }
+    const claimAddress = '5ChQuE91nkwu2C2LF3j8BUgBfCcrMR7CLDaR9rvLmyZLJ7hq';
+    const evmAddress = '0x1bff005918ab80f8f3b16fdd0722654a62aeb0d2ffd11b895fc305a77dee6933';
+    const claim = newMerkleTree.claims[claimAddress];
+    if (claim) {
+        const tx4 = await merkleDistributor.claim(evmAddress, claim.tokens, claim.cumulativeAmounts, claim.index, claim.cycle, claim.proof, {
+            gasPrice: ethParams.txGasPrice,
+            gasLimit: ethParams.txGasLimit,
+        });
+        await tx4.wait();
+        console.log('Claimed tDOT for ' + claimAddress);
     }
 }
